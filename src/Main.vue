f4dd54a4d96f18c5a4ac54b40dd754b530f2ebe0
park <winifred3546@gmail.com>
2023-01-14 17:20:10 +0900

    캘린더 PJT

---
+++ src/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Main Page</h1>
+        <Header></Header>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+</style>
